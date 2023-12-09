8056cda8579c1bc15a535b7b13b89f3250fc3b75
RahulJ000 <153193347+RahulJ000@users.noreply.github.com>
2023-12-09 11:30:49 +0530

    Update p8 telegram.py

--- p8 telegram.py
+++ p8 telegram.py
@@ -12,22 +12,22 @@ GPIO.setwarnings(False)
 GPIO.setup(red,GPIO.OUT)
 GPIO.output(red,0)
 def action(msg):
-chat_id = msg['chat']['id']
-command = msg['text']
-print ('Got command: %s' %command)
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print ('Got command: %s' %command)
 if 'On' in command:
-message="Turn On"
-message=message+" red"
-GPIO.output(red,1)
-bot.sendMessage(chat_id,message)
+    message="Turn On"
+    message=message+" red"
+    GPIO.output(red,1)
+    bot.sendMessage(chat_id,message)
 if 'Off' in command:
-message="Turn Off"
-message=message+" red"
-GPIO.output(red,0)
-bot.sendMessage(chat_id, message)
-bot = telepot.Bot('') #paste your bot token here.
-print(bot.getMe())
-MessageLoop(bot,action).run_as_thread()
-print ('I am listening...')
+    message="Turn Off"
+    message=message+" red"
+    GPIO.output(red,0)
+    bot.sendMessage(chat_id, message)
+    bot = telepot.Bot('') #paste your bot token here.
+    print(bot.getMe())
+    MessageLoop(bot,action).run_as_thread()
+    print ('I am listening...')
 while 1:
-time.sleep(10)
+    time.sleep(10)
